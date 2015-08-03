5750bbe667d265c9291e19d615dc1084224b2ce8
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-08-04 00:29:42 +0900

    Make lint happy

--- request.go
+++ request.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 )
 
+// StateToken returns securely-generated random string. It is suitable for state parameter.
 func StateToken() string {
 	randBuf := make([]byte, 30)
 	rand.Read(randBuf)
